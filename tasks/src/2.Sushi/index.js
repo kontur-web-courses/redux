19e80c388e81fc6dc095c7a5a0d5ca13f1dfa9ac
digi <digi@skbkontur.ru>
2019-07-08 13:01:36 +0500

    2.2.Logging is solved

--- tasks/src/2.Sushi/index.js
+++ tasks/src/2.Sushi/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import 'regenerator-runtime/runtime';
 import './styles.css';
 import Page from './constants/Page';
@@ -11,8 +12,19 @@ import ErrorBoundary from './components/ErrorBoundary';
 import Navigation from './containers/Navigation';
 import Pages from './containers/Pages';
 import { rootReducer } from './reducers';
+import * as actionTypes from './actionTypes';
 import products from './api/products';
 import Api from './api';
+
+const customMiddleWare = ({ getState, dispatch }) => next => action => {
+  // console.log(action.type);
+  if (action.type === actionTypes.NAVIGATE_TO_PAGE) {
+    if (getState().page === Page.menu) {
+      return next({ ...action, page: Page.cart });
+    }
+  }
+  return next(action);
+};
 
 const productsAllIds = products.map(p => p.id);
 const productsById = products.reduce(
@@ -31,7 +43,11 @@ const preloadedState = {
 
 const api = new Api({ baseUrl: 'http://sampleserviceurl?foo=bar' });
 
-const store = createStore(rootReducer, preloadedState);
+const store = createStore(
+  rootReducer,
+  preloadedState,
+  applyMiddleware(customMiddleWare, logger)
+);
 
 class App extends React.Component {
   render() {
