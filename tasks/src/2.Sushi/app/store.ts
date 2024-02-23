0371a20e6be712df94fc300ebb82b1abf11704fe
Бондаренко Кирилл Владимирович <kbondarenko@skbkontur.ru>
2024-02-23 20:03:47 +0500

    2.4.Logging is solved

--- tasks/src/2.Sushi/app/store.ts
+++ tasks/src/2.Sushi/app/store.ts
@@ -4,7 +4,9 @@ import {Page} from '../constants/Page';
 import {Status} from '../constants/Status';
 import {configureStore, PreloadedState, StateFromReducersMapObject} from '@reduxjs/toolkit';
 import {productsReducer} from '../features/products/productsSlice';
-import {pageReducer} from '../features/navigation/navigationSlice';
+import {navigateTo, pageReducer} from '../features/navigation/navigationSlice';
+import {Middleware} from 'redux';
+import logger from 'redux-logger';
 
 export const api = new Api({baseUrl: 'http://sampleserviceurl?foo=bar'});
 
@@ -30,8 +32,22 @@ const preloadedState: PreloadedState<RootState> = {
 
 export type RootState = StateFromReducersMapObject<typeof reducer>;
 
+const customMiddleWare: Middleware =
+	({getState}) =>
+	(next) =>
+	(action) => {
+		// console.log(action.type);
+		if (action.type === navigateTo.type) {
+			if (getState().page === Page.menu) {
+				next({...action, page: Page.cart});
+			}
+		}
+		next(action);
+	};
+
 export const store = configureStore({
 	reducer,
 	preloadedState,
+	middleware: [customMiddleWare, logger],
 });
 export type AppDispatch = typeof store.dispatch;
